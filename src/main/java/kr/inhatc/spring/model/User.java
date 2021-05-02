7a5e4de7530205108db8e2b2e886181e33dc7816
곽성욱 <seong_wook@gwagseong-ug-ui-MacBookAir.local>
2021-05-02 22:34:41 +0900

    디자인 변경

--- src/main/java/kr/inhatc/spring/model/User.java
+++ src/main/java/kr/inhatc/spring/model/User.java
@@ -1,9 +1,11 @@
 package kr.inhatc.spring.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 import lombok.Data;
 
@@ -13,10 +15,21 @@ public class User {
    @Id
    @GeneratedValue(strategy= GenerationType.IDENTITY)
    private int id;
+   
+   @Column(nullable = false)
    private String username;
+   
+   @Column(nullable = false)
    private String password;
+
+   @Column(nullable = false)
    private String name;
+   
+   @Column(nullable = false)
    private String department;
+   
    private String email;
+   
+   @Column(nullable = false)
    private String role;   //ROLE_STUDENT, ROLE_PROF
 }
